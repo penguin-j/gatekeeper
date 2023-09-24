e74e33a360e45b4ff6b643c910084db3feeaa5f8
j <penguinjinbei@gmail.com>
2023-09-25 03:58:37 +0900

    add: Pre Sign-up Lambda新規作成

---
+++ src/confirm-sign-up/handler.js
@@ -0,0 +1,15 @@
+module.exports.execute = async (event) => {
+  // Confirm the user
+  event.response.autoConfirmUser = true
+  // Set the email as verified if it is in the request
+  if (event.request.userAttributes.hasOwnProperty("email")) {
+    event.response.autoVerifyEmail = true
+  }
+
+  // Set the phone number as verified if it is in the request
+  if (event.request.userAttributes.hasOwnProperty("phone_number")) {
+    event.response.autoVerifyPhone = true
+  }
+
+  return event
+}
